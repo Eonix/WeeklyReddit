cc13d355a425c3f3ce152cdf5ba3e67f2100d431
Martin A <Eonix@users.noreply.github.com>
2017-12-23 16:27:56 +0100

    Added debugging test run argument.

--- WeeklyReddit/Program.cs
+++ WeeklyReddit/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using FluentScheduler;
 using Microsoft.Extensions.Configuration;
@@ -6,104 +7,109 @@ using WeeklyReddit.Services;
 
 namespace WeeklyReddit
 {
-  public class Program
-  {
-    public static void Main()
+    public class Program
     {
-      try
-      {
-        var builder = new ConfigurationBuilder();
-        builder.AddJsonFile(@"C:\dev\secrets\WeeklyReddit\appsettings.json", true); // dev settings.
-        builder.AddEnvironmentVariables();
-
-        var configuration = builder.Build();
-
-        JobManager.JobException += x => Log($"An unhandled exception occurred.{Environment.NewLine}{x.Exception}");
-        JobManager.UseUtcTime();
-
-        Log("Weekly Reddit Started!");
-
-        var registry = new Registry();
-        registry.Schedule(() =>
-            GenerateNewsletterAsync(configuration).ConfigureAwait(false).GetAwaiter().GetResult()).ToRunEvery(0)
-          .Weeks()
-          .On(DayOfWeek.Friday).At(12, 0);
-
-        JobManager.Initialize(registry);
-        JobManager.Start();
-      }
-      catch (Exception exception)
-      {
-        Log("Unhandled exception!");
-        Log(exception.ToString());
-      }
-
-      Log("Press enter to quit.");
-      Console.ReadLine();
-
-      JobManager.Stop();
-      Log("Program ended.");
-    }
-
-    private static void Log(string message)
-    {
-      Console.WriteLine($"{DateTime.UtcNow}: {message}");
-    }
-
-    private static async Task GenerateNewsletterAsync(IConfiguration configuration)
-    {
-      Log("Generating newsletter.");
-      const string title = "Weekly Newsletter of Reddit";
-
-      var redditOptions = new RedditOptions
-      {
-        Username = configuration["reddit:username"],
-        Password = configuration["reddit:password"],
-        ClientId = configuration["reddit:clientId"],
-        ClientSecret = configuration["reddit:clientSecret"]
-      };
-
-      Log("Fetching content...");
-      using (var redditClient = await RedditClient.CreateAsync(redditOptions))
-      {
-        var subreddits = await redditClient.GetSubredditsTopPosts();
-        var trendings = await redditClient.GetTrendings();
-
-        var formatterOptions = new FormatterOptions
+        public static void Main(string[] args)
         {
-          Subreddits = subreddits,
-          Title = title,
-          IssueDate = DateTime.Today,
-          Trendings = trendings
-        };
+            try
+            {
+                var builder = new ConfigurationBuilder();
+                builder.AddJsonFile(@"C:\dev\secrets\WeeklyReddit\appsettings.json", true); // dev settings.
+                builder.AddEnvironmentVariables();
 
-        Log("Generating email...");
-        var html = await DataFormatter.GenerateHtmlAsync(formatterOptions);
+                var configuration = builder.Build();
 
-        var emailOptions = new EmailOptions
-        {
-          Password = configuration["smtpSettings:password"],
-          Username = configuration["smtpSettings:username"],
-          SmtpServer = configuration["smtpSettings:server"],
-          SmtpPort = Convert.ToInt32(configuration["smtpSettings:port"]),
-        };
+                JobManager.JobException += x => Log($"An unhandled exception occurred.{Environment.NewLine}{x.Exception}");
+                JobManager.UseUtcTime();
 
-        Log("Sending email...");
-        using (var emailClient = new EmailClient(emailOptions))
-        {
-          var content = new EmailContent
-          {
-            Content = html,
-            Subject = $"{title} // {DateTime.Today.ToLongDateString()}",
-            FromName = title,
-            FromAddress = configuration["emailSettings:fromAddress"],
-            To = configuration["emailSettings:toAddress"]
-          };
-          await emailClient.SendAsync(content);
+                Log("Weekly Reddit Started!");
+
+                var registry = new Registry();
+                registry.Schedule(() =>
+                    GenerateNewsletterAsync(configuration).ConfigureAwait(false).GetAwaiter().GetResult()).ToRunEvery(0)
+                  .Weeks()
+                  .On(DayOfWeek.Friday).At(12, 0);
+
+                JobManager.Initialize(registry);
+                JobManager.Start();
+
+                if (args.Contains("--testrun"))
+                {
+                    GenerateNewsletterAsync(configuration).Wait();
+                }
+            }
+            catch (Exception exception)
+            {
+                Log("Unhandled exception!");
+                Log(exception.ToString());
+            }
+
+            Log("Press enter to quit.");
+            Console.ReadLine();
+
+            JobManager.Stop();
+            Log("Program ended.");
         }
-      }
 
-      Log("Newsletter sent!");
+        private static void Log(string message)
+        {
+            Console.WriteLine($"{DateTime.UtcNow}: {message}");
+        }
+
+        private static async Task GenerateNewsletterAsync(IConfiguration configuration)
+        {
+            Log("Generating newsletter.");
+            const string title = "Weekly Newsletter of Reddit";
+
+            var redditOptions = new RedditOptions
+            {
+                Username = configuration["reddit:username"],
+                Password = configuration["reddit:password"],
+                ClientId = configuration["reddit:clientId"],
+                ClientSecret = configuration["reddit:clientSecret"]
+            };
+
+            Log("Fetching content...");
+            using (var redditClient = await RedditClient.CreateAsync(redditOptions))
+            {
+                var subreddits = await redditClient.GetSubredditsTopPosts();
+                var trendings = await redditClient.GetTrendings();
+
+                var formatterOptions = new FormatterOptions
+                {
+                    Subreddits = subreddits,
+                    Title = title,
+                    IssueDate = DateTime.Today,
+                    Trendings = trendings
+                };
+
+                Log("Generating email...");
+                var html = await DataFormatter.GenerateHtmlAsync(formatterOptions);
+
+                var emailOptions = new EmailOptions
+                {
+                    Password = configuration["smtpSettings:password"],
+                    Username = configuration["smtpSettings:username"],
+                    SmtpServer = configuration["smtpSettings:server"],
+                    SmtpPort = Convert.ToInt32(configuration["smtpSettings:port"]),
+                };
+
+                Log("Sending email...");
+                using (var emailClient = new EmailClient(emailOptions))
+                {
+                    var content = new EmailContent
+                    {
+                        Content = html,
+                        Subject = $"{title} // {DateTime.Today.ToLongDateString()}",
+                        FromName = title,
+                        FromAddress = configuration["emailSettings:fromAddress"],
+                        To = configuration["emailSettings:toAddress"]
+                    };
+                    await emailClient.SendAsync(content);
+                }
+            }
+
+            Log("Newsletter sent!");
+        }
     }
-  }
 }
